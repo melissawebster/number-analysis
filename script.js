a086e74e2b4c84e282ba69f2321dbc2822e2f7ad
Melissa Webster <imelissawebster@gmail.com>
2023-04-25 13:39:16 -0300

    javascript update

--- script.js
+++ script.js
@@ -16,6 +16,12 @@ function add() {
     num.focus()  // keeps cursor in input after sent
 } 
 
+function reset() {
+    document.getElementById('id_number').reset()
+    // add reset all
+
+}
+
 
 function analyze() {
     let res = document.querySelector('div#result')
@@ -23,6 +29,28 @@ function analyze() {
     if (array.length == 0) {
         alert('You should add at least one number to the list!')
     } else {
-        res.innerHTML = `[ ${array} ]`
+        let tot = array.length
+        let highest = array[0]
+        let lowest = array[0]
+        let sum = 0
+        let average = 0
+
+        for(let pos in array) {
+            if (array[pos] > highest) {
+                highest = array[pos]
+            }
+            if (array[pos] < lowest) {
+                lowest = array[pos]
+            }
+            sum += array[pos]
+        }
+        average = sum / array.length
+
+        res.innerHTML = `<p> [ ${array} ] </p>`
+        res.innerHTML += `Input numbers: ${tot} <br>`
+        res.innerHTML += `Lowest number: ${lowest} <br>`
+        res.innerHTML += `Highest number: ${highest} <br>`
+        res.innerHTML += `Sum: ${sum} <br>`
+        res.innerHTML += `Average: ${average.toFixed(2)} <br>`
     }
 }
